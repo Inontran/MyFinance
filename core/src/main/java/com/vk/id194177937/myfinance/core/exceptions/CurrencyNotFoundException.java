d919c7c3aa302f9601f80c4bdef7a5f72ea819b7
Inontran <braginsasha1995@mail.ru>
2016-07-04 16:56:14 +0900

    implementing class CurrencyNotFoundException

---
+++ core/src/main/java/com/vk/id194177937/myfinance/core/exceptions/CurrencyNotFoundException.java
@@ -0,0 +1,10 @@
+package com.vk.id194177937.myfinance.core.exceptions;
+
+/**
+ * Created by Inontran on 04.07.16.
+ */
+public class CurrencyNotFoundException extends Exception {
+    public CurrencyNotFoundException() {
+        super("Currency Not Found");
+    }
+}
