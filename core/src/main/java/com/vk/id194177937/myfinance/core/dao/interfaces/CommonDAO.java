da0c98fa65c493ab1a45c602b28cb481a2a29bbd
Inontran <braginsasha1995@mail.ru>
2016-07-07 17:41:39 +0900

    comment forgot

--- core/src/main/java/com/vk/id194177937/myfinance/core/dao/interfaces/CommonDAO.java
+++ core/src/main/java/com/vk/id194177937/myfinance/core/dao/interfaces/CommonDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 /**
  * Created by Inontran on 07.07.16.
  */
+//общие действия с БД для всех объектов
 public interface CommonDAO<T> {
     List<T> getAll();
     T get(long id);
